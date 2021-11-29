9e1f7249022eeffcd90a5f9fd7027506c17bfceb
Suraj Singla <suraj.singla@mckinleyrice.co>
2021-11-29 19:38:32 +0530

    BSC keyring accounts management class and project updates

--- src/helpers/utils/getNetwork.js
+++
@@ -1,9 +0,0 @@
-const { bsc_network: { MAINNET, TESTNET } } = require('../../config/index')
-
-function getNetworkDetails(_network) {
-    return _network === TESTNET.NETWORK ?
-        { url: 'https://data-seed-prebsc-1-s1.binance.org:8545', chainId: TESTNET.CHAIN_ID } :
-        { url: `https://bsc-dataseed1.binance.org`, chainId: MAINNET.CHAIN_ID }
-}
-
-module.exports = getNetworkDetails
